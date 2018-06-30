3955cf27c181c7c24eee292944f05d94ae3c965f
Emily Miles <emiles4@gmail.com>
2018-06-30 04:37:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,9 +8,9 @@ def position_taken? (board, index)
 end
 end
 
-def valid_move?(board, index) 
-  if position_taken?(board, index) 
-    false 
-  elsif index.between?(0,8) true 
-  end 
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    false
+  elsif index.between?(0,8) true
+  end
 end
